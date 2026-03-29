dd6b7a71cd4cd57ff25cf95790561f722bd8e3a5
GABRIEL SENA <batistafilmes@Sena-MacBook-Pro.local>
2026-03-29 15:00:53 -0300

    Update: set cloud.png as favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
   title: "stick to the plan.",
   description: "Explore, Conecte e Apoie a nossa Jornada.",
   icons: {
-    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>☁️</text></svg>',
+    icon: '/cloud.png',
   },
 };
 
